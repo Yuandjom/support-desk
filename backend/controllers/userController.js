d1ee71249b46636857bb63d60eff6e703f2995fa
Yuandjom <ziyangjohn@gmail.com>
2022-12-11 16:22:41 +0800

    Error & Exception Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,31 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+//this will handle all the try catch 
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// @description Register a new user
+// @route       /api/users
+// @access      Public
+const registerUser = asyncHandler(async(req, res) => {
+    //in req.body is where the data is going to be stored
+    console.log(req.body) //need to send the body parser middleware (else it will be undefined)
+
+    //destructure from the req.body
+    const {name , email, password} = req.body
+
+    //validation 
+    if(!name  || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields') //we can create a custom error handler in a middleware
+    }
+
+    res.send('Register Route')
+})
+
+// @description Login a user
+// @route       /api/users/login
+// @access      Public
+const loginUser = asyncHandler(async(req, res) => {
     res.send('Login Route')
-}
+})
 
 //once you export, remember to bring it in to the userRoutes file 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@
 const express = require('express')
 //bring in the environment variable 
 const dotenv = require('dotenv').config()
+//get the errorHandler function from the errorMiddleware (then pass it into the app.use)
+const {errorHandler} = require('./middleware/errorMiddleware')
 //this is from the env file
 const PORT = process.env.PORT || 5000
 
 //initialise the app variable
 const app = express()
+
+//this is the middleware to have the body parser
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //creating a route with express
 //use postman to test for the response
@@ -21,6 +27,8 @@ app.get('/', (req, res) => {
 //to use the route 
 //the end point is /api/users
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler) //this wont show in production, only show in development
 
 //listen to a specific port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
